55dd76a6313dd69c658547285b2b0bebe4ac94fd
axel tavares <tavaresaxel95@gmail.com>
2024-04-02 21:09:09 -0400

    updated model files and fixed some errors

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
-const Comment=require("./Comment")
+
 
 User.hasMany(Post, {
   foreignKey: 'user_id',
@@ -12,23 +12,5 @@ Post.belongsTo(User, {
 });
 
 
-Post.hasMany(Comment, {
-  foreignKey: 'post_id',
-  onDelete: 'CASCADE'
-});
 
-Comment.belongsTo(Post, {
-  foreignKey: 'post_id'
-});
-
-
-User.hasMany(Comment, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
-});
-
-Comment.belongsTo(User, {
-  foreignKey: 'user_id'
-});
-
-module.exports = { User, Post, Comment };
+module.exports = { User, Post};
